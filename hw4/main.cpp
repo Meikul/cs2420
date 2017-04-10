ba00cd6327bbb911e4c70ded00570f7e7e20c1e1
Meikul <meikul.harris@gmail.com>
2017-04-10 14:56:48 -0600

    main for graders

--- hw4/main.cpp
+++ hw4/main.cpp
@@ -8,31 +8,50 @@ int toInt(string str);
 int main()
 {
   HashTable inventory;
+  cout << "    After Initialization" << endl;
   inventory.printTable();
-  inventory.insert(6520, "Apples");
-  inventory.insert(3340, "Mini Bananas");
-  inventory.insert(2414, "Durian");
-  inventory.insert(2093, "Beets");
-  
-  inventory.printTable();
-  
-  inventory.remove(3340);
-  inventory.insert(7782, "Starfruit");
-  inventory.insert(3323, "Dragonfruit");
-  inventory.printTable();
-  return 0;
-}
+  inventory.insert(4514,"alfalfa sprouts");
+  inventory.insert(4131,"apple fuji");
+  inventory.insert(4133,"apple gala");
+  inventory.insert(4017,"apple granny smith");
+  inventory.insert(4218,"apricots");
 
-int toInt(string str)
-{
-  int out=0;
-  for(unsigned int it=0; it<str.length(); it++)
-  {
-    int cur = (int)str[it];
-    if(cur > 47 && cur < 58)
-      out+=(cur-48)*(10*(str.length()-1));
-    else
-      return -1;
-  }
-  return out;
+  cout << "    Last entry before rehash" << endl;
+  inventory.printTable();
+
+  inventory.insert(3175,"beans haba dry");
+  cout << "    Entry causes rehash" << endl;
+  inventory.insert(4528,"beans haba fresh");
+  inventory.insert(3173,"beans lentils");
+  inventory.insert(3170,"beans peruano");
+  inventory.insert(3172,"beans pinto");
+  inventory.insert(3177,"beans red");
+  inventory.insert(4539,"beets bunch");
+  inventory.insert(4540,"beets loose");
+  inventory.insert(4065,"bell pepper green");
+  inventory.insert(4088,"bell pepper red");
+  inventory.insert(4239,"black berries");
+  inventory.insert(4545,"bok choy");
+  inventory.insert(4060,"broccoli");
+  inventory.insert(4550,"brussel sprouts");
+  inventory.insert(4069,"cabbage green");
+  inventory.insert(4556,"cabbage Napa");
+  cout << "    All 20 inserted" << endl;
+  inventory.printTable();
+
+  cout << "   Finding Three" << endl;
+  cout << inventory.find(4556).key << " " << inventory.find(4556).name << endl;
+  cout << inventory.find(4239).key << " " << inventory.find(4239).name << endl;
+  cout << inventory.find(4218).key << " " << inventory.find(4218).name << endl;
+
+
+  inventory.remove(4060);
+  inventory.remove(4550);
+  inventory.remove(3177);
+  cout << "    After removing 3" << endl;
+  inventory.printTable();
+
+
+
+  return 0;
 }
